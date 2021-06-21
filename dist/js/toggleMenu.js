76322c7b56a021cafe0a6bfcd113c70c56362f40
Yaiza16 <yaizavallejo16@gmail.com>
2021-06-21 13:47:12 +0200

    toggleMenu

--- dist/js/toggleMenu.js
+++ dist/js/toggleMenu.js
@@ -1,3 +1,9 @@
 export default function toggleMenu(){
-    
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.getElementById('nav');
+
+    hamburger.addEventListener('click', () =>{
+        nav.classList.toggle('nav--opened')
+        hamburger.classList.toggle('hamburger-closed')
+    })
 }
